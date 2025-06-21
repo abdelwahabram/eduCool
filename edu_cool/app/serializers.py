8b33161d0194b7e58fbb584cf9a5ec387d1a15b9
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2025-06-21 20:54:24 +0300

    created serializers for: Course(), Announcements(), Comment(), User() models

---
+++ edu_cool/app/serializers.py
@@ -0,0 +1,53 @@
+from app.models import Course, Announcement, Comment
+
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+
+	tutor = serializers.HyperlinkedIdentityField(read_only=True, view_name='user-detail')
+
+	announcements = serializers.HyperlinkedIdentityField(read_only=True, view_name='announcement-list', lookup_url_kwarg = 'course_pk')
+	
+	students = serializers.HyperlinkedIdentityField(read_only=True, view_name='student-list', lookup_url_kwarg='course_pk')
+	
+	class Meta():
+
+		model = Course
+		
+		fields = ['id', 'url', 'tutor', 'title', 'announcements', 'students']
+
+
+class AnnouncementSerializer(serializers.HyperlinkedModelSerializer):
+    
+	course = serializers.HyperlinkedIdentityField(read_only=True, view_name='course-detail')
+
+	comments = serializers.HyperlinkedIdentityField(read_only=True, view_name='comment-list', lookup_url_kwarg='announcement_pk')
+
+	class Meta():
+		model = Announcement
+		fields = ['id', 'url', 'course', 'title', 'content', 'time', 'attachment', 'comments']
+    
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+	
+	author = serializers.HyperlinkedIdentityField(read_only=True, view_name='user-detail')
+	
+	announcement = serializers.HyperlinkedIdentityField(read_only=True, view_name='announcement-detail')
+	
+	class Meta(object):
+		
+		model = Comment
+		
+		fields = ['id', 'url', 'author', 'announcement', 'content', 'time']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+	class Meta():
+		
+		model = User
+		
+		fields = ['id', 'url', 'username', 'my_courses']
