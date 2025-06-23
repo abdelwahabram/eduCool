3e914ab3249803492a46734199ac7d920aaaef73
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2025-06-23 22:44:23 +0300

    added student-list, comment-list, comment-detail views urls + fixed a typo in announcement-detail url + added linespaces to make it readable

--- edu_cool/app/urls.py
+++ edu_cool/app/urls.py
@@ -13,7 +13,15 @@ main_router.register(r'users', views.UserViewSet, basename="user")
 
 urlpatterns = [
     re_path(r'^courses/(?P<course_pk>\d+)/announcements/?$', views.AnnouncementViewSet.as_view({'get': 'list', 'post': 'create'}), name='announcement-list'),
-    re_path(r'^announcements/(?P<pk>\d+)/?$', views.AnnouncementViewSet.as_view({'get': 'retieve'}), name='announcement-detail'),
+    
+    re_path(r'^announcements/(?P<pk>\d+)/?$', views.AnnouncementViewSet.as_view({'get': 'retrieve'}), name='announcement-detail'),
+    
+    re_path(r'^courses/(?P<course_pk>\d+)/students/?$', views.StudentViewSet.as_view({'get': 'list'}), name='student-list'),
+    
+    re_path(r'^announcements/(?P<announcement_pk>\d+)/comments/?$', views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
+    
+    re_path(r'^comments/(?P<pk>\d+)/?$', views.CommentViewSet.as_view({'get': 'retrieve'}), name='comment-detail'),
+    
     path('', include(main_router.urls)),
 
 ]
