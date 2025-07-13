f253ee078f9067ed7a8fe5b130330d96b23653e4
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2025-07-13 18:54:46 +0300

    installed drf_simplejwt + djoser

--- edu_cool/edu_cool/settings.py
+++ edu_cool/edu_cool/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'djoser',
+    'app',
 ]
 
 MIDDLEWARE = [
