4bfba6f79e6da3adfc4c2254988b1f665bb29ad8
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2025-11-21 09:59:50 +0200

    - created login view that add the tokens to the request httponly cookie - created logout view that logs the users out by removing the cookie from the response object - created another view to refresh the auth token when it expires

--- edu_cool/app/views.py
+++ edu_cool/app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 from app.models import Course, Enrollment, Announcement, Comment
 
-from app.serializers import CourseSerializer, EnrollmentSerializer, AnnouncementSerializer, UserSerializer, CommentSerializer
+from app.serializers import CourseSerializer, EnrollmentSerializer, AnnouncementSerializer, UserSerializer, CommentSerializer, UserLogInSerializer
 
 from app import permissions
 
@@ -12,11 +12,31 @@ from rest_framework import viewsets
 
 from rest_framework.decorators import action
 
-from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.exceptions import NotFound, PermissionDenied, AuthenticationFailed
 
 from rest_framework.response import Response
 
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.views import APIView
+
+from rest_framework.generics import GenericAPIView
+
+from rest_framework import status
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from app.cookies import set_access_cookie, set_refresh_cookie, scan_refresh_cookie, remove_cookie
+
+from rest_framework.renderers import TemplateHTMLRenderer
+
+from django.contrib.auth import authenticate
+
+from django.middleware.csrf import rotate_token
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from rest_framework_simplejwt.serializers import TokenBlacklistSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -258,3 +278,203 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 	serializer_class = UserSerializer
 
 	permission_classes = [permissions.IsUserSession]
+
+
+# class LogInAPIView(APIView):
+	# 	# TODO: Define requests here
+
+	# 	# serializer_class = UserLogInSerializer
+
+	# 	# renderer_classes = [TemplateHTMLRenderer]
+
+	# 	# template_name = "login.html"
+
+	# 	def post(self, request):
+
+	# 		serializer = UserLogInSerializer(data=request.data)
+
+	# 		print(request.data)
+
+	# 		if not serializer.is_valid():
+	# 			print("not valid")
+	# 			return Response({"invalid": "invalid"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+	# 		# print(serializer.validated_data)
+
+	# 		username = serializer.validated_data['username']
+	# 		password = serializer.validated_data['password']
+
+	# 		user = authenticate(request, username=username, password=password)
+
+	# 		if not user:
+	# 			# raise 
+	# 			return Response({"invalid": "invalid"}, status=status.HTTP_400_BAD_REQUEST)
+
+	# 		response = Response({}, status = status.HTTP_200_OK)
+
+	# 		refresh_token = RefreshToken.for_user(user)
+
+	# 		access_token = refresh_token.access_token
+
+
+	# 		set_token_cookies(response, str(access_token), str(refresh_token))
+	# 		# set cookies
+			
+	# 		return response
+
+
+		# def get(self, request):
+
+		# 	return Response({"serializer": UserLogInSerializer()})
+
+	# class MODEL_NAMEViewSet(viewsets.ModelViewSet):
+	#     queryset = MODEL_NAME.objects.all()
+	#     serializer_class = SERIALIZER_CLASS
+	# # "username" : "yyy", "password":"456"
+
+
+# class AuthViewSet(viewsets.ViewSet):
+	# """
+	# Example empty viewset demonstrating the standard
+	# actions that will be handled by a router class.
+
+	# If you're using format suffixes, make sure to also include
+	# the `format=None` keyword argument for each action.
+	# """
+
+	# # def list(self, request):
+	# # 	pass
+
+	# serializer_class = UserLogInSerializer
+
+	# def create(self, request):
+
+	# 	serializer = UserLogInSerializer(data=request.data)
+
+	# 	# print(request.data)
+
+	# 	if not serializer.is_valid():
+	# 		# print("not valid")
+	# 		return Response({"invalid": "invalid"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+	# 	# print(serializer.validated_data)
+
+	# 	username = serializer.validated_data['username']
+	# 	password = serializer.validated_data['password']
+
+	# 	user = authenticate(request, username=username, password=password)
+
+	# 	if not user:
+	# 		# raise 
+	# 		return Response({"invalid": "invalid"}, status=status.HTTP_400_BAD_REQUEST)
+
+	# 	response = Response({}, status = status.HTTP_200_OK)
+
+	# 	refresh_token = RefreshToken.for_user(user)
+
+	# 	access_token = refresh_token.access_token
+
+
+	# 	set_token_cookies(response, str(access_token), str(refresh_token))
+	# 	# set cookies
+		
+	# 	return response
+
+
+	# def retrieve(self, request):
+
+	# 	refresh_token = extract_token_from_cookie(request)
+
+	# 	serializer = TokenBlacklistSerializer(data={"refresh": refresh_token})
+
+	# 	try:
+	# 		serializer.is_valid(raise_exceptions=True)
+	# 	except e:
+	# 		raise e
+
+	# 	response = Response({}, status.HTTP_200_OK)
+
+	# 	remove_token(response)
+
+	# 	return response
+
+
+# we might list these methods under the UserViewSet() with overriding the get_serializer depending upon the method, but it's fine like that
+
+class LogInView(GenericAPIView):
+
+	serializer_class = UserLogInSerializer
+
+	def post(self, request):
+
+		serializer = self.get_serializer(data=request.data)
+
+		serializer.is_valid()
+
+		username = serializer.validated_data['username']
+		
+		password = serializer.validated_data['password']
+
+		user = authenticate(request, username=username, password=password)
+
+		if not user or not user.is_active:
+			raise AuthenticationFailed(detail="no active user found")
+		
+		response = Response({}, status = status.HTTP_200_OK)
+
+		refresh_token = RefreshToken.for_user(user)
+
+		access_token = refresh_token.access_token
+
+		set_refresh_cookie(response, str(refresh_token))
+		
+		set_access_cookie(response, str(access_token))
+
+		rotate_token(request)
+		
+		return response
+
+
+class LogOutView(APIView):
+
+	def post(self, request):
+
+		refresh_token = scan_refresh_cookie(request)
+
+		print(refresh_token)
+
+		serializer = TokenBlacklistSerializer(data={"refresh": refresh_token})
+
+		serializer.is_valid(raise_exception=True)
+
+		response = Response({}, status.HTTP_200_OK)
+
+		remove_cookie(response)
+
+		return response
+
+
+class RefreshTokenView(TokenRefreshView):
+
+	def post(self, request, *args, **kwargs):
+
+		refresh_token = scan_refresh_cookie(request)
+
+		serializer = self.get_serializer(data = {"refresh": refresh_token})
+			
+		serializer.is_valid(raise_exceptions=True)
+
+		response = Response({}, status.HTTP_200_OK)
+
+		access_token = serializer.validated_data['access_token']
+		
+		refresh_token = serializer.validated_data['refresh_token']
+
+		set_access_cookie(response, str(access_token))
+		
+		set_refresh_cookie(response, str(refresh_token))
+		
+		return response
+
