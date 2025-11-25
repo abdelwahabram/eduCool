cdc04a9c7aa906385af7fb6b6be646a2264912cd
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2025-11-25 14:02:27 +0200

    configured pagination class

--- edu_cool/edu_cool/settings.py
+++ edu_cool/edu_cool/settings.py
@@ -145,7 +145,12 @@ REST_FRAMEWORK = {
         'app.authenticate.JWTCookieAuth',
 		
 		# 'rest_framework_simplejwt.authentication.JWTAuthentication',
-	)
+	),
+
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+
 
 }
 
