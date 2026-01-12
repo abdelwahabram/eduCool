37c5c810f05d061455fd5518b655edf0e6ab5b39
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2026-01-12 20:34:46 +0200

    the node server logins to the django server and add the jwt token to the websocket connection

---
+++ sfu/index.js
@@ -0,0 +1,71 @@
+console.log('server running frfrfrfrfrfr')
+
+// send get req to login
+// extract the csrf token
+// extract the creds from the docker secret
+// send post req to login
+// extract the cookie from the response obj
+// open ws connection with cookie included
+
+
+import axios from 'axios';
+
+// import cookie from 'cookie'
+
+
+function parseCookie(header){
+
+	let keyValue = header.split(';')[0] + ";"
+
+	return keyValue
+
+	// let key = keyValue[0]
+	// let value = keyValue[1]
+
+	// return {key: key, value: value}
+
+}
+
+
+axios.post('http://django:8000/login/', 
+	{username: 'tyler_67', password: 'asd123ZXC'}).then(function (response) {
+		// handle success
+		console.log(response.headers['set-cookie']);
+		console.log(response.data);
+
+		let cookies = ''
+
+		for (let cookie of response.headers['set-cookie']){
+
+			let parsedCookie = parseCookie(cookie)
+
+			cookies = cookies + parsedCookie
+
+			// cookies[parsedCookie['key']] = parsedCookie['value']
+
+
+		}
+
+		console.log(cookies)
+		return cookies
+
+	}).then(function(cookies){
+
+		console.log(cookies)
+
+		const chatSocket = new WebSocket(
+			'ws://' + 'django:8000' + '/ws/chat/',
+
+			{'headers': {
+            'Cookie': cookies
+				}
+         }
+      )
+
+		chatSocket.onopen = function(e){
+			console.log('skibidi connection')
+		}
+
+  })
+
+
