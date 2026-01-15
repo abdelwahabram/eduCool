6953cc18befe0162ecdf5944b809bbfe2c7da716
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2026-01-15 23:09:59 +0200

    updated dj auth middleware to reject anonymous calls, handled some exceptions and updated the user query

--- edu_cool/app/channels_auth.py
+++ edu_cool/app/channels_auth.py
@@ -4,58 +4,65 @@ from django.contrib.auth.models import AnonymousUser
 
 from rest_framework_simplejwt.tokens import AccessToken
 
+from rest_framework_simplejwt.exceptions import TokenError
+
 from django.contrib.auth.models import User
 
+from rest_framework.exceptions import NotAuthenticated
 
 
 class ChannelsJwtAuth:
 
-    def __init__(self, app):
-        # Store the ASGI application we were passed
-        self.app = app
+	def __init__(self, app):
+		# Store the ASGI application we were passed
+		self.app = app
 
-        self.cookie_name = settings.SIMPLE_JWT["AUTH_COOKIE_ACCESS"]
-
-
-    async def __call__(self, scope, receive, send):
-
-        self.scope = dict(scope)
-
-        token = await self.get_token()
-
-        # print("token: ", token)
-
-        if token:
-
-        	token_instance = AccessToken(token)
-        	user_id = token_instance['user_id']
-
-        	# user_name = token_instance['username']
-        	# print("user:", token_instance.payload)
-
-        	user = await User.objects.aget(pk=user_id)
-        	# instead of hitting the db, consider overriding the token obtain pair serializer to add tthe user name to the token payload
-
-        	user_name = user.username
-
-        	# print(user_name)
-
-        	scope['user_id'] = user_id
-
-        	scope['user_name'] = user_name
-
-        else:
-
-        	scope['user'] = AnonymousUser()
-        	# this is just a basic version to add the user info to the scope
-
-        return await self.app(scope, receive, send)
+		self.cookie_name = settings.SIMPLE_JWT["AUTH_COOKIE_ACCESS"]
 
 
-    async def get_token(self):
+	async def __call__(self, scope, receive, send):
 
-    	print("cookies", self.scope['cookies'])
+		self.scope = dict(scope)
 
-    	token = self.scope["cookies"].get(self.cookie_name)
+		token = await self.get_token()
 
-    	return token
+		if not token:
+
+			raise NotAuthenticated('user not authenticated')
+
+		try:
+
+			token_instance = AccessToken(token)
+
+		except:
+
+			TokenError()
+
+		user_id = token_instance['user_id']
+
+		try:
+
+			user = await User.objects.prefetch_related('my_courses').aget(pk=user_id)
+		
+		except:
+
+			raise AuthenticationFailed(' no user found')
+
+		scope['user'] = user
+
+
+		# else:
+		# 	# scope['user'] = AnonymousUser()
+		# 	# this is just a basic version to add the user info to the scope
+
+		return await self.app(scope, receive, send)
+
+
+	async def get_token(self):
+
+		token = self.scope["cookies"].get(self.cookie_name)
+
+		return token
+
+# for a better design we could rebuild this in a 'chain of responsibility' manner like dj channels AuthMiddlewareStack
+# where each class handle one job and pass to the next
