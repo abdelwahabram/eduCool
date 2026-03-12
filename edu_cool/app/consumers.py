15f5b0472acde0b4a43c45884c77d81aef2b1472
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2026-03-12 18:04:41 +0200

    - added all the peers of the same room to a group on the django channels consumer - enabled sending a group message to all room from the server - assigned 'is_sfu' flag to the scope after authenticating the sfu

--- edu_cool/app/consumers.py
+++ edu_cool/app/consumers.py
@@ -20,6 +20,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
         if self.room_name is None and self.scope['user'].is_superuser and self.scope['user'].username == get_docker_secret('server_cred_username', 'admin'):
             
+            self.scope['is_sfu'] = True
+
             await self.channel_layer.group_add(self.sfu_group, self.channel_name)
             
             await self.accept()
@@ -42,6 +44,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
             raise DenyConnection('only tutors and students could join this rrom')
 
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
+
         await self.accept()
 
 
@@ -58,7 +62,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         message['sender_channel'] = self.channel_name
 
-        if not message['receiver_channel']:
+
+        if not self.scope.get('is_sfu', None):
 
             message['user_name'] = self.scope['user'].username
 
@@ -68,10 +73,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 self.sfu_group, {"type": "chat.message", "message": message}
             )
 
-        else:
+            return
+
+        if message.get(receiver_channel, None) != None:
 
             await self.channel_layer.send(
                 message['receiver_channel'], {"type": "chat.message", "message": message}
+            )
+
+        elif message.get('group', None) != None:
+
+            if not self.scope['is_sfu_group']:
+                return
+
+            await self.channel_layer.group_send(
+                message['group'], {"type": "chat.message", "message": message}
             )
 
 
