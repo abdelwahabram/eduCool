1de92bda23698055b24b5eb7f2735158b78a4e92
abdelwahabram <63767622+abdelwahabram@users.noreply.github.com>
2026-04-26 21:00:55 +0300

    removed the list brackets for proper decouple format configuration

--- edu_cool/edu_cool/settings.py
+++ edu_cool/edu_cool/settings.py
@@ -33,7 +33,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1'], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 # Application definition
 
